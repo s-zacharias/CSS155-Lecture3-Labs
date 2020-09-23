709da3feba3f86c590c7dea6646b14d8fc7c5447
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-09-23 17:14:51 -0500

    fix lab 2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -11,18 +11,18 @@ namespace Lab2
 
             Console.WriteLine("Uh oh. I understand that your lamp isn't working. " +
                 "Let me try to help. Please enter Yes or No to the following questions.");
-            Console.WriteLine("Is the lamp plugged in?: " + pluggedIn);
+            Console.WriteLine("Is the lamp plugged in?: ", pluggedIn);
 
-            if (pluggedIn == "No" || pluggedIn = "no")
+            if (pluggedIn == "No")
             {
                 Console.WriteLine("Plug in the lamp.");
-            } else if (pluggedIn == "Yes" || pluggedIn == "yes")
+            } else if (pluggedIn == "Yes")
             {
-                Console.WriteLine("Is the bulb burnt out?: " + burntOut);
-                if (burntOut == "No" || burntOut == "no")
+                Console.WriteLine("Is the bulb burnt out?: ", burntOut);
+                if (burntOut == "No")
                 {
                     Console.WriteLine("Please repair your lamp.");
-                } else if (burntOut == "Yes" || burntOut == "yes")
+                } else if (burntOut == "Yes")
                 {
                     Console.WriteLine("Please replace the lightbulb.");
                 }
